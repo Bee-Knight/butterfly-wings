e3fdc4feba8c1a389e18a991552961326adf3095
ley <xiajinlei@foxmail.com>
2024-06-20 00:53:25 +0800

    fix list sort

--- src/components/dpostlist/dpostlist.jsx
+++ src/components/dpostlist/dpostlist.jsx
@@ -18,48 +18,83 @@ class DPostList extends React.Component {
       return <Loading/>
     }
 
-    const element = posts.map((post, index) => {
-      if (index === posts.length - 1 && posts.length % 2 === 0) {
-        return (
-          <View key={post.id} className="d-post-list-card">
-            <DPostCard
-              type={post.type}
-              title={post.title}
-              url={post.cover}
-              desc={post.desc}
-              theme={post.theme}
-              lastModified={post.lastModified}
-              mode={post.mode}
-              playersCount={post.playersCount}
-              key={post.id}
-              id={post.id}
-            />
-          </View>
-        )
+    // const element = posts.map((post, index) => {
+    //   if (index === posts.length - 1 && posts.length % 2 === 0) {
+    //     return (
+    //       <View key={post.id} className="d-post-list-card">
+    //         <DPostCard
+    //           type={post.type}
+    //           title={post.title}
+    //           url={post.cover}
+    //           desc={post.desc}
+    //           theme={post.theme}
+    //           lastModified={post.lastModified}
+    //           mode={post.mode}
+    //           playersCount={post.playersCount}
+    //           key={post.id}
+    //           id={post.id}
+    //         />
+    //       </View>
+    //     )
+    //   } else {
+    //     return (
+    //       <View key={post.id} className="d-post-list-card">
+    //         <DPostCard
+    //           type={post.type}
+    //           title={post.title}
+    //           url={post.cover}
+    //           desc={post.desc}
+    //           theme={post.theme}
+    //           lastModified={post.lastModified}
+    //           mode={post.mode}
+    //           playersCount={post.playersCount}
+    //           key={post.id}
+    //           id={post.id}
+    //         />
+    //         <View style="height:12px"/>
+    //       </View>
+    //     )
+    //   }
+    // })
+
+    let leftList = []
+    let rightList = []
+    posts.map((post, index) => {
+      let card =
+        <View key={post.id} className="d-post-list-card">
+          <DPostCard
+            type={post.type}
+            title={post.title}
+            url={post.cover}
+            desc={post.desc}
+            theme={post.theme}
+            lastModified={post.lastModified}
+            mode={post.mode}
+            playersCount={post.playersCount}
+            key={post.id}
+            id={post.id}
+          />
+          <View style="height:12px"/>
+        </View>
+      if (index % 2 === 0) {
+        leftList.push(card)
       } else {
-        return (
-          <View key={post.id} className="d-post-list-card">
-            <DPostCard
-              type={post.type}
-              title={post.title}
-              url={post.cover}
-              desc={post.desc}
-              theme={post.theme}
-              lastModified={post.lastModified}
-              mode={post.mode}
-              playersCount={post.playersCount}
-              key={post.id}
-              id={post.id}
-            />
-            <View style="height:12px"/>
-          </View>
-        )
+        rightList.push(card)
       }
     })
 
     return (
-      <View className="d-post-list">
-        {element}
+      // <View className="d-post-list">
+      //   {element}
+      // </View>
+
+      <View className="d-post-list-v2">
+        <View className="d-post-list-left">
+          {leftList}
+        </View>
+        <View className="d-post-list-right">
+          {rightList}
+        </View>
       </View>
     )
   }
